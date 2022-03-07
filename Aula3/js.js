91880636b7382945d70a8a27ec9c88d85d438eac
lleo05 <100773337+lleo05@users.noreply.github.com>
2022-03-07 12:44:44 +0000

    Add files via upload

---
+++ Aula3/js.js
@@ -0,0 +1,64 @@
+function generate(size, lowest, highest) {
+	var numbers = [];
+	for(var i = 0; i < size; i++) {
+		var add = true;
+		var randomNumber = Math.floor(Math.random() * highest) + 1;
+		for(var y = 0; y < highest; y++) {
+			if(numbers[y] == randomNumber) {
+				add = false;
+			}
+		}
+		if(add) {
+			numbers.push(randomNumber);
+		} else {
+			i--;
+		}
+	}
+  
+	var highestNumber = 0;
+	for(var m = 0; m < numbers.length; m++) {
+		for(var n = m + 1; n < numbers.length; n++) {
+			if(numbers[n] < numbers[m]) {
+				highestNumber = numbers[m];
+				numbers[m] = numbers[n];
+				numbers[n] = highestNumber;
+			}
+		}
+	}
+  
+	document.getElementById("numbers").innerHTML = numbers.join(" - ");
+}
+
+//--------------------------------
+//--------------------------------
+
+function generateEstrelas(size, lowest, highest) {
+	var estrelas = [];
+	for(var i = 0; i < size; i++) {
+		var add = true;
+		var randomNumber = Math.floor(Math.random() * highest) + 1;
+		for(var y = 0; y < highest; y++) {
+			if(estrelas[y] == randomNumber) {
+				add = false;
+			}
+		}
+		if(add) {
+			estrelas.push(randomNumber);
+		} else {
+			i--;
+		}
+	}
+  
+	var highestNumber = 0;
+	for(var m = 0; m < estrelas.length; m++) {
+		for(var n = m + 1; n < estrelas.length; n++) {
+			if(estrelas[n] < estrelas[m]) {
+				highestNumber = estrelas[m];
+				estrelas[m] = estrelas[n];
+				estrelas[n] = highestNumber;
+			}
+		}
+	}
+  
+	document.getElementById("estrelas").innerHTML = estrelas.join(" - ");
+}
